72930b312f19863e3bbba75acf85e9626ef6908d
Ubuntu <ubuntu@ip-172-31-17-56.us-east-2.compute.internal>
2020-05-11 14:34:48 +0000

    completed django tutorial

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,44 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
+
+class ChoiceInline(admin.TabularInline): # StackedInline for vertical list
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+    # Display fields on the questions page
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    # Adds a filter sidebar that lets people filter the change list by pub_date
+    list_filter = ['pub_date']
+    # Adds a search box at top of change list
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
+
+""" class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin) """
+
+""" class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text'] """
 
 # Tell admin that Question objects have an admin interface
-admin.site.register(Question)
+#admin.site.register(Question)
+
+# Just this line creates an "add choice" form that includes a dropdown to pick the 
+# corresponding question.
+# admin.site.register(Choice)
+
+
+
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -28,6 +28,11 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
+    # Adding attributes to was_published recently to change how it's shown in list display
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
